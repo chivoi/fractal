7468df4d7ec72d858ae5d24e4a189cab03974023
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2020-07-01 21:43:50 +1000

    Breaking test for Nastya to fix

--- spec/fractal_spec.rb
+++ spec/fractal_spec.rb
@@ -3,7 +3,7 @@ require 'fractal'
 describe Fractal do
   describe '#generate' do
     it 'will return fractal' do
-      expect(Fractal.new.generate).to eq "fractal"
+      expect(Fractal.new.generate).to eq "its not fractal"
     end
   end
 end
