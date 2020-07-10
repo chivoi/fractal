e982b34243400e4d886cc00364f88030db1b7899
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2020-07-10 10:24:35 +1000

    Added test for stale point

--- spec/limit_detector_spec.rb
+++ spec/limit_detector_spec.rb
@@ -3,23 +3,45 @@ require 'limit_detector'
 describe LimitDetector do
   
   describe '#iterations' do
-    it 'will tell how many iterations needed to go to infinity' do
-    	#-----------------------------------
-    	# set up
-    	constant = ComplexNumber.new(2, 0)
-    	function = lambda { |z| z.add(constant) } # z**2 + c
+  	context 'when there is a limit' do
+	    it 'will tell how many iterations needed to go to infinity' do
+	    	#-----------------------------------
+	    	# set up
+	    	constant = ComplexNumber.new(2, 0)
+	    	function = lambda { |z| z.add(constant) } # z**2 + c
 
-    	limit_detector = LimitDetector.new(
-    		max_iterations: 10, 
-    		max_absolute_value: 100.0, 
-    		function: function
-    	)
+	    	limit_detector = LimitDetector.new(
+	    		max_iterations: 10, 
+	    		max_absolute_value: 100.0, 
+	    		function: function
+	    	)
 
-    	# calculation
-    	number = ComplexNumber.new(0, 0)
-    	result = limit_detector.iterations(number)
-    	#-----------------------------------
-    	expect(result).to eq 5
-    end
+	    	# calculation
+	    	number = ComplexNumber.new(0, 0)
+	    	result = limit_detector.iterations(number)
+	    	#-----------------------------------
+	    	expect(result).to eq 5
+	    end
+	  end
+
+	  context 'when point stays on the same place' do
+			it 'will return the max_iterations' do
+	    	#-----------------------------------
+	    	# set up
+	    	function = lambda { |z| z } # identity
+
+	    	limit_detector = LimitDetector.new(
+	    		max_iterations: 3, 
+	    		max_absolute_value: 100.0, 
+	    		function: function
+	    	)
+
+	    	# calculation
+	    	number = ComplexNumber.new(0, 0)
+	    	result = limit_detector.iterations(number)
+	    	#-----------------------------------
+	    	expect(result).to eq 3
+	    end
+	  end
   end
 end
