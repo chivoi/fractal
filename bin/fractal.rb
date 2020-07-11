a7773fd2f9276c8f44b4084316ff101ed63cd090
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2020-07-11 10:30:14 +1000

    Added some stats, drawing fractal now

--- bin/fractal.rb
+++ bin/fractal.rb
@@ -4,12 +4,9 @@ require './lib/limit_detector'
 require './lib/screen'
 require './lib/renderer'
 
-puts "Fractal ASCII generator v1.0"
-puts "============================"
-
-COLS = 40
-ROWS = 20
-MAX_ITERATIONS = 12
+COLS = 20
+ROWS = 40
+MAX_ITERATIONS = 11
 # value from here https://mathworld.wolfram.com/MandelbrotSet.html
 CONSTANT = ComplexNumber.new(-0.75, 0.1)
 
@@ -30,19 +27,40 @@ renderer = Renderer.new(
 limit_detector = LimitDetector.new(
 	max_iterations: MAX_ITERATIONS,
 	max_absolute_value: 4,
-	function: lambda { |z| z.multiply(z) + add(CONSTANT) }
+	function: lambda { |z| z.multiply(z) + CONSTANT }
 )
 
-# for x in 0..COLS
-# 	for y in 0..ROWS
-xs = [0..COLS]
-ys = [0..ROWS]
-points = xs.product(ys)
-puts "Points: #{points}"
+# xs = [0..COLS]
+# ys = [0..ROWS]
+# points = xs.product(ys)
+# chars = points.each.map do |x, y|
+# 	puts "x=#{x}, y=#{y}"
+# end
+# puts "Points: #{points}"
 
-chars = points.each.map do |x, y|
-	puts "x=#{x}, y=#{y}"
+output = []
+for x in 0..COLS
+	for y in 0..ROWS
+		z = screen.convert(x, y)
+		iterations = limit_detector.iterations(z)
+		cell = renderer.render(iterations)
+		output.append(cell)
+	end
+	output.append("\n")
 end
+
+puts "Fractal ASCII generator v1.0"
+puts "============================"
+
+puts "origin=[#{screen.origin_re}, #{screen.origin_im}]" +
+	" max_iterations: #{MAX_ITERATIONS}"
+
+puts "real=[#{screen.real_width}, #{screen.real_height}]" +
+	" view=[#{screen.view_width}, #{screen.view_height}]"
+
+puts "formula: z=z^2 + #{CONSTANT}"
+puts "#{output.join}"
+
 
 
 
--- lib/renderer.rb
+++ lib/renderer.rb
@@ -8,8 +8,15 @@ class Renderer
 	end
 
 	def render(iteration)
+		if not iteration.between?(0, max_iterations) 
+			message = "iteration #{iteration} should be between 0 and #{max_iterations}"
+			raise ArgumentError.new(message) 
+		end
+
 		index = (iteration / iterations_per_char).round
-		chars[index]
+		result = chars[index]
+		puts "index:#{index} result:#{result}"
+		result
 	end
 
 	def chars_count
