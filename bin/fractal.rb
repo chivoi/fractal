f05e2d708aadc28d4df846c6d02f396735e9a21b
Nastya Lastovirya <souhlasim@gmail.com>
2020-07-22 18:27:23 +1000

    Made controls.

--- bin/fractal.rb
+++ bin/fractal.rb
@@ -9,16 +9,12 @@ require "./lib/limit_detector"
 require "./lib/renderer"
 require "./lib/screen"
 
-puts "Fractal generator version 0.0.1"
-puts "==============================="
-puts "Created by chivoii and kirghoff"
-
 #chars = "─━│┃┄┅┆┇┈┉┊┋┌┍┎┏┐┑┒┓└┕┖┗┘┙┚┛├┝┞┟┠┡┢┣┤┥┦┧┨┩┪┫┬┭┮┯┰┱┲┳┴┵┶┷┸┹┺┻┼┽┾┿╀╁╂╃╄╅╆╇╈╉╊╋"
 chars = "@MBHENR\#KWXDFPQASUZbdehx*8Gm&04LOVYkpq5Tagns69owz$CIu23Jcfry%1v7l+it[]{}?j|()=~!-/<>\"^_';,:`. ".reverse
 #chars = '.,-~:;=!*\#$@ '
 #puts "Chars length is #{chars.length}"
 width = TTY::Screen.cols - 5
-height = TTY::Screen.rows
+height = TTY::Screen.rows - 9
 
 renderer = Renderer.new(
 	max_iterations: chars.length, 
@@ -36,48 +32,65 @@ limit_detector = LimitDetector.new(
 
 screen = Screen.new(
 	origin: ComplexNumber.new(-1.5, 1.5), 
-	#origin_re: -1.5,
-	#origin_im: 1.5,
 	real_width: 3.0, 
 	real_height: 3.0,
 	view_width: width,
 	view_height: height
 )
 
-puts "Origin = #{screen.origin.re} + #{screen.origin.im}i"
-puts "Press w,a,s,d to change shape, x to exit"
+loop do
 
-key = STDIN.getch
+	system "clear"
+	puts "Fractal generator version 0.0.1"
+	puts "==============================="
+	puts "Created by chivoii and kirghoff"
 
-case key
-	when "w"
-		screen.origin.im += screen.real_height / 10
-	when "s"
-		screen.origin.im -= screen.real_height / 10
-	when "a"
-		screen.origin.re -= screen.real_width / 10
-	when "d"
-		screen.origin.re += screen.real_width / 10
-	else 
-		puts "Change fractal shape with wsad keys"
-end
+	puts "Press w,a,s,d to change shape, x to exit"
+	puts "Constant = #{constant.re} + #{constant.im}i"
 
-output = []
+	output = []
 
-for y in 0..height
-	for x in 0..width
-		number = screen.convert(x, y)
-		iterations = limit_detector.iterations(number)
-		character = renderer.render(iterations)
-		output << character 
-		# puts "#{x}, #{y}, #{number}, #{iterations}, #{character}"
+	for y in 0..height
+		for x in 0..width
+			number = screen.convert(x, y)
+			iterations = limit_detector.iterations(number)
+			character = renderer.render(iterations)
+			output << character 
+			# puts "#{x}, #{y}, #{number}, #{iterations}, #{character}"
+		end
+		output << "\n" 
 	end
-	output << "\n" 
-end
 
-box = TTY::Box.frame(width: TTY::Screen.cols, height: TTY::Screen.rows, border: :thick) do
-	"#{output.join}"
+	print TTY::Box.frame "#{output.join}"
+
+	#print box
+
+	key = STDIN.getch
+
+	case key
+		when "s"
+			screen.origin.im += screen.real_height / 10
+		when "w"
+			screen.origin.im -= screen.real_height / 10
+		when "d"
+			screen.origin.re -= screen.real_width / 10
+		when "a"
+			screen.origin.re += screen.real_width / 10
+		when "i"
+			constant.im += constant.absolute_value / 10
+		when "k"
+			constant.im -= constant.absolute_value / 20
+		when "j"
+			constant.re += constant.absolute_value / 10
+		when "l"
+			constant.re -= constant.absolute_value / 20
+		when "x"
+			break
+		else 
+			puts "Change fractal shape with wsad keys"
+	end
+
+puts "Baiiiiieeee."
+
 end
-	
-print box
 
